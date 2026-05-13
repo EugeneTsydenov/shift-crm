5015556b3a34844fb2eb8405efa129f6437cf389
EugeneTsydenov <ecydenov110@gmail.com>
2026-05-13 23:56:05 +0700

    feat: add transaction not found exception

---
+++ app/src/main/java/org/example/exception/TransactionNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class TransactionNotFoundException extends RuntimeException {
+    public TransactionNotFoundException(Long id) {
+        super("Transaction not found with id: " + id);
+    }
+}
