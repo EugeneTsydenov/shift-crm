dc9c499d2ae6e815903ffb783c986dd3e246b06d
EugeneTsydenov <ecydenov110@gmail.com>
2026-05-13 22:08:22 +0700

    feat: add transaction request and response dto

--- app/src/main/java/org/example/model/dto/transaction/TransactionResponse.java
+++ app/src/main/java/org/example/model/dto/transaction/TransactionResponse.java
@@ -1,5 +1,14 @@
 package org.example.model.dto.transaction;
 
-public class TransactionResponse {
-    
-}
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.example.model.enums.PaymentType;
+
+public record TransactionResponse (
+    Long Id,
+    Long sellerId, 
+    BigDecimal amount,
+    PaymentType paymentType,
+    LocalDateTime transactionDate
+) {}
