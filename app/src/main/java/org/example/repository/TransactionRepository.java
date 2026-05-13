b44f0d1ca70c1dd5d674a1abc6c1fda96f6e99d3
EugeneTsydenov <ecydenov110@gmail.com>
2026-05-13 23:57:20 +0700

    feat: add findAllBySellerId to transaction repository

--- app/src/main/java/org/example/repository/TransactionRepository.java
+++ app/src/main/java/org/example/repository/TransactionRepository.java
@@ -1,7 +1,10 @@
 package org.example.repository;
 
+import java.util.List;
+
 import org.example.model.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllBySellerId(Long sellerId);
 }
