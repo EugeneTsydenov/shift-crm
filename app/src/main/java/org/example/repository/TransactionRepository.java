d3491915f97e785b347618e63b621fe0dba69298
EugeneTsydenov <ecydenov110@gmail.com>
2026-05-14 14:18:34 +0700

    fix: find only active transactions

--- app/src/main/java/org/example/repository/TransactionRepository.java
+++ app/src/main/java/org/example/repository/TransactionRepository.java
@@ -12,35 +12,41 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findAllBySellerId(Long sellerId);
+	List<Transaction> findAllBySellerId(Long sellerId);
 
-    @Query("""
-                SELECT t.seller.id AS id, t.seller.name AS name, SUM(t.amount) AS totalAmount
-                FROM Transaction t
-                WHERE t.transactionDate >= :from AND t.transactionDate <= :to
-                AND t.seller.deletedAt IS NULL
-                GROUP BY t.seller.id, t.seller.name
-                ORDER BY SUM(t.amount) DESC
-            """)
-    List<SellerTotalProjection> findMostProductiveSeller(@Param("from") LocalDateTime from,
-            @Param("to") LocalDateTime to);
+	@Query("SELECT t FROM Transaction t WHERE t.seller.deletedAt IS NULL")
+	List<Transaction> findAllActive();
 
-    @Query("""
-                SELECT t.seller.id AS id, t.seller.name AS name, SUM(t.amount) AS totalAmount
-                FROM Transaction t
-                WHERE t.transactionDate >= :from AND t.transactionDate <= :to
-                AND t.seller.deletedAt IS NULL
-                GROUP BY t.seller.id, t.seller.name
-                HAVING SUM(t.amount) < :amount
-            """)
-    List<SellerTotalProjection> findSellersWithTotalBelow(@Param("from") LocalDateTime from,
-            @Param("to") LocalDateTime to, @Param("amount") BigDecimal amount);
+	@Query("SELECT t FROM Transaction t WHERE t.id = :id AND t.seller.deletedAt IS NULL")
+	Optional<Transaction> findActiveById(@Param("id") Long id);
 
-    @Query("""
-            SELECT t.transactionDate
-            FROM Transaction t
-            WHERE t.seller.id = :sellerId
-            ORDER BY t.transactionDate asc
-            """)
-    List<LocalDateTime> findTransactionDatesBySellerId(@Param("sellerId") Long sellerId);
+	@Query("""
+			    SELECT t.seller.id AS id, t.seller.name AS name, SUM(t.amount) AS totalAmount
+			    FROM Transaction t
+			    WHERE t.transactionDate >= :from AND t.transactionDate <= :to
+			    AND t.seller.deletedAt IS NULL
+			    GROUP BY t.seller.id, t.seller.name
+			    ORDER BY SUM(t.amount) DESC
+			""")
+	List<SellerTotalProjection> findMostProductiveSeller(@Param("from") LocalDateTime from,
+			@Param("to") LocalDateTime to);
+
+	@Query("""
+			    SELECT t.seller.id AS id, t.seller.name AS name, SUM(t.amount) AS totalAmount
+			    FROM Transaction t
+			    WHERE t.transactionDate >= :from AND t.transactionDate <= :to
+			    AND t.seller.deletedAt IS NULL
+			    GROUP BY t.seller.id, t.seller.name
+			    HAVING SUM(t.amount) < :amount
+			""")
+	List<SellerTotalProjection> findSellersWithTotalBelow(@Param("from") LocalDateTime from,
+			@Param("to") LocalDateTime to, @Param("amount") BigDecimal amount);
+
+	@Query("""
+			SELECT t.transactionDate
+			FROM Transaction t
+			WHERE t.seller.id = :sellerId
+			ORDER BY t.transactionDate asc
+			""")
+	List<LocalDateTime> findTransactionDatesBySellerId(@Param("sellerId") Long sellerId);
 }
