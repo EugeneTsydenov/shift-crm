043ec6d74f389739adc74dca66cb3fe7c6240b62
EugeneTsydenov <ecydenov110@gmail.com>
2026-05-14 12:41:04 +0700

    refactor: del useless imports

--- app/src/main/java/org/example/exception/GlobalExceptionHandler.java
+++ app/src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -1,10 +1,7 @@
 package org.example.exception;
 
-import java.util.stream.Collectors;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
